f6fd1e9e0686e210e72c65285d0c39327dcd9865
Gusar82 <rusgusa82@gmail.com>
2022-04-10 00:08:11 +0500

    completing task#2

--- task_oop.py
+++ task_oop.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +23,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -24,22 +41,40 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
-
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Best', 'Student', 'male')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Git']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+worst_student = Student("Worst", 'Student', 'female')
+worst_student.courses_in_progress += ['Python']
+worst_student.courses_in_progress += ['Git']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer = Reviewer('Cool', 'Reviewer')
+cool_reviewer.courses_attached += ['Python']
+
+ineffectual_reviewer = Reviewer('Ineff', 'Reviewer')
+ineffectual_reviewer.courses_attached += ['Git']
+
+cool_lecturer = Lecturer('Cool', 'Lecturer')
+cool_lecturer.courses_attached += ['Python']
+
+ineffectual_lecturer = Lecturer('Ineff', 'Lecturer')
+ineffectual_lecturer.courses_attached += ['Git']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(worst_student, 'Python', 5)
+cool_reviewer.rate_hw(worst_student, 'Git', 5)  # ошибка
+ineffectual_reviewer.rate_hw(best_student, 'Git', 10)
+ineffectual_reviewer.rate_hw(worst_student, 'Git', 10)
+
+best_student.rate_hw(cool_lecturer, 'Python', 8)
+worst_student.rate_hw(cool_lecturer, 'Python', 10)
+best_student.rate_hw(ineffectual_lecturer, 'Git', 5)
+best_student.rate_hw(ineffectual_lecturer, 'Python', 5)  # ошибка
+worst_student.rate_hw(ineffectual_lecturer, 'Git', 8)
 
 print(best_student.grades)
+print(worst_student.grades)
+print(cool_lecturer.grades)
+print(ineffectual_lecturer.grades)
